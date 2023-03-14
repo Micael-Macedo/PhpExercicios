dc4bc07b4065f5066fee6406ca36448d0bd995ae
Micael Macedo <micaelm2009@hotmail.com>
2023-03-14 20:46:08 -0300

    feat: add escapes sequences

---
+++ ex004/index.php
@@ -0,0 +1,21 @@
+<?php 
+    $nome = 'Micael';
+    $sobrenome = 'Macedo';
+    echo "$nome $sobrenome \n";
+
+    const ESTADO = "BA";
+    echo "Moro na " .ESTADO . "\n";
+    echo "Estamos no ano de " .date('Y');
+
+    $nome = "Rodrigo";
+    $snome = "Nogueira";
+
+    echo "$nome \"Minotauro\" $snome";
+
+    //escapes sequences
+    // \n Nova linha
+    // \t Tab
+    // \\ barra invertida
+    // \$ Sinal de cifrão
+    // \u{} Codepoint Unicode
+?>
