cad156330f8168bbf60200c2f26e1cf8f74dea2d
Micael-Macedo <micaelm2009@hotmail.com>
2023-03-20 09:34:44 -0300

    fix: get values from api

--- AtvConversorMoedaApi/index.php
+++ AtvConversorMoedaApi/index.php
@@ -3,7 +3,9 @@ $real = 500.75;
 $diaAtual = date('m/d/Y');
 $diaAtual = str_replace("/","-", $diaAtual);
 $url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='03-16-2023'&\$top=100&\$skip=0&\$format=json";
-$json = file_get_contents($url);
-$data = json_decode($json);
-echo $data->value;
+$data = file_get_contents($url);
+$json = json_decode($data);
+$valorCompra =  json_encode($json->value[0]->cotacaoCompra);
+$valorVenda =  json_encode($json->value[0]->cotacaoVenda);
+echo("Valor para compra: $valorCompra \nValor para venda: $valorVenda");
 ?>
