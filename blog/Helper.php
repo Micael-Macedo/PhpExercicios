ff9f4256a3ce8adad36719a7c39e6bf6407a1711
Micael-Macedo <micaelm2009@hotmail.com>
2023-03-21 12:08:58 -0300

    feat: add blog project

---
+++ blog/Helper.php
@@ -0,0 +1,16 @@
+<?php
+date_default_timezone_set('America/Bahia');
+function saudacao(){
+    $hora = date("H");
+    var_dump($hora);
+    if($hora >= 0 and $hora <= 5){
+        return "Boa madrugada";
+    }elseif($hora >= 6 and $hora < 12){
+        return "Bom dia";
+    }
+    elseif($hora >= 12 and $hora < 18){
+        return "Boa Tarde";
+    }else{
+        return "Boa noite";
+    }
+}
---
+++ blog/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'app/config.php';
+include 'Helper.php';
+
+echo saudacao();
