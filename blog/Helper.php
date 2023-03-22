4842179b4b245b8220a8ec4bb30639301a6b4d7e
Micael Macedo <micaelm2009@hotmail.com>
2023-03-22 19:31:43 -0300

    feat: add filters

--- blog/Helper.php
+++ blog/Helper.php
@@ -1,20 +1,21 @@
 <?php
+
 date_default_timezone_set('America/Bahia');
 /**
  * Realiza uma saudação conforme o horario da bahia
  * @return string saudação baseada no horário
  */
-function saudacao(){
+function saudacao()
+{
     $hora = date("H");
     var_dump($hora);
-    if($hora >= 0 and $hora <= 5){
+    if ($hora >= 0 and $hora <= 5) {
         return "Boa madrugada";
-    }elseif($hora >= 6 and $hora < 12){
+    } elseif ($hora >= 6 and $hora < 12) {
         return "Bom dia";
-    }
-    elseif($hora >= 12 and $hora < 18){
+    } elseif ($hora >= 12 and $hora < 18) {
         return "Boa Tarde";
-    }else{
+    } else {
         return "Boa noite";
     }
 }
@@ -24,13 +25,14 @@ function saudacao(){
  * @param int $limite quantidade máxima de caracteres
  * @return string texto resumido
  */
-function resumirTexto(string $texto, int $limite){
+function resumirTexto(string $texto, int $limite)
+{
     $texto = trim($texto);
-    if(strlen($texto) > $limite){
-        $textoResumido = substr($texto,0,$limite);
+    if (strlen($texto) > $limite) {
+        $textoResumido = substr($texto, 0, $limite);
         return $textoResumido;
     }
-    return $texto."...";
+    return $texto . "...";
 }
 
 function contarTempo(string $data)
@@ -39,28 +41,34 @@ function contarTempo(string $data)
     $tempo = strtotime($data);
     $diferenca = $agora - $tempo;
     $segundos = $diferenca;
-    $minutos = round($diferenca/60);
-    $horas = round($diferenca/3600);
-    $dias = round($diferenca/86400);
-    $semanas = round($diferenca/604800);
-    $meses = round($diferenca/2419200);
-    $anos = round($diferenca/29030400);
-    if($segundos <= 60){
+    $minutos = round($diferenca / 60);
+    $horas = round($diferenca / 3600);
+    $dias = round($diferenca / 86400);
+    $semanas = round($diferenca / 604800);
+    $meses = round($diferenca / 2419200);
+    $anos = round($diferenca / 29030400);
+    if ($segundos <= 60) {
         return 'agora';
-    }elseif($minutos <= 60){
+    } elseif ($minutos <= 60) {
         return $minutos == 1 ? 'há 1 minuto' : "há $minutos minutos";
-    }elseif($horas <= 24){
+    } elseif ($horas <= 24) {
         return $horas == 1 ? 'há 1 hora' : "há $horas horas";
-    }
-    elseif($dias <= 7){
+    } elseif ($dias <= 7) {
         return $dias == 1 ? 'há ontem' : "há $dias dias";
-    }
-    elseif($semanas <= 4){
+    } elseif ($semanas <= 4) {
         return $semanas == 1 ? 'há 1 semana' : "há $semanas semanas";
-    }
-    elseif($meses <= 12){
+    } elseif ($meses <= 12) {
         return $meses == 1 ? 'há 1 mês' : "há $meses meses";
-    }elseif($anos <= 1){
+    } elseif ($anos <= 1) {
         return $anos == 1 ? 'há 1 ano' : "há $anos anos";
     }
+}
+
+function validarEmail(string $email): bool
+{
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+function validarUrl(string $url): bool
+{
+    return filter_var($url, FILTER_VALIDATE_URL);
 }
--- blog/index.php
+++ blog/index.php
@@ -5,3 +5,8 @@ include 'Helper.php';
 $texto = "Bem vindo ao meu blog";
 echo resumirTexto($texto, 12);
 echo contarTempo("2022-03-22 11:20:00");
+if(validarEmail("micael@hotmail.com")){
+    echo "Email validado com sucesso";
+}else{
+    echo "Email inválido";
+}
